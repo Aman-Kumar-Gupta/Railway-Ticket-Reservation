5b6aef77ce123110344bc2f20138cd549406f78d
Aman <amankr250804@gmail.com>
2025-04-14 13:11:44 +0530

    .

--- frontend/src/components/ListPassengers.jsx
+++ frontend/src/components/ListPassengers.jsx
@@ -96,7 +96,6 @@ const ListPassengers = () => {
                                         type="date"
                                         value={journeyDate}
                                         onChange={(e) => setJourneyDate(e.target.value)}
-                                        min={new Date().toISOString().split('T')[0]}
                                         className="w-full pl-10 pr-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
                                         required
                                     />
